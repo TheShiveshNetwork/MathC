37c2c8e3296a082d3bb3f5382ecdee9765cc9912
TheShiveshNetwork <shiveshtiwari0@gmail.com>
2023-11-01 19:30:46 +0530

    initial-setup

--- main.c
+++ main.c
@@ -1,4 +1,50 @@
 #include<stdio.h>
+#include<stdbool.h>
+
+// int reRun(bool terminate) {
+//     if (!terminate) {
+//         Setup();
+//     } else {
+//         return 0;
+//     }
+// }
+
+void SimpleCalc() {
+    printf("This is simple calc!\n");
+}
+
+void QuadraticCalc() {
+    printf("This is quadratic calc!\n");
+}
+
+void Setup() {
+    int number_fun;
+    printf("[?] What do you want to calculate?\n");
+    printf("(Press 1) Simple Mathematics\n");
+    printf("(Press 2) Solve Quadratic Equation\n\n");
+
+    printf("Enter a number from the above list: ");
+    scanf("%d", &number_fun);
+
+    // printf("%d\n", number_fun);
+    if (number_fun == 1) {
+        SimpleCalc();
+    }
+    if (number_fun == 2) {
+        QuadraticCalc();
+    }
+
+    char terminateChar;
+    printf("\n[?] Terminate the program? (Press y / n) ");
+    scanf("%s", &terminateChar);
+    // printf("\nTerminate: %c\n", terminateChar);
+
+    if (terminateChar == 'n') {
+        printf("\n\n====================\n\n");
+        terminateChar = 'y';
+        Setup();
+    }
+}
 
 int main () {
     char logo[] = " __     __     ____    _______   _     _           _____\n"
@@ -9,5 +55,7 @@ int main () {
     ;
     printf(logo);
 
+    Setup();
     return 0;
 }
+
